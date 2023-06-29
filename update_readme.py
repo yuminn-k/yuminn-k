f2b606c561ef9eab9c26501adaccdca4dd9b8072
devYuMinKim <gimyumin40@gmail.com>
2023-06-30 03:14:30 +0900

    🎨 refactor : Modify README.md update logic

--- update_readme.py
+++ update_readme.py
@@ -3,18 +3,16 @@ import json
 with open("output.json", "r") as json_file:
     post_data = json.load(json_file)
 
-new_section = "## Recent Blog Posts\n"
+new_section = "<h2>Blog Posts</h2>\n"
 for post in post_data:
     new_section += f"- [{post['title']}]({post['link']})\n"
 
 with open("README.md", "r") as readme:
     readme_content = readme.read()
 
-readme_content = readme_content.split("<!-- BLOGPOSTS:START -->")[0] + \
-                 "<!-- BLOGPOSTS:START -->" + "\n" + \
+readme_content = readme_content.split("<h2>Blog Posts</h2>")[0] + \
                  new_section + \
-                 "<!-- BLOGPOSTS:END -->" + "\n" + \
-                 readme_content.split("<!-- BLOGPOSTS:END -->")[1]
+                 "".join(readme_content.split("<h2>Blog Posts</h2>")[1:])
 
 with open("README.md", "w") as readme:
     readme.write(readme_content)
