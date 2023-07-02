5d5e84d7f3ec8712e4d5a70859e5b684c80d293e
devYuMinKim <gimyumin40@gmail.com>
2023-07-02 16:19:19 +0900

    Update blog_posts_scraper.py

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -7,19 +7,28 @@ urls = ["https://devyuminkim.github.io/lifelog/", "https://devyuminkim.github.io
 output_data = []
 
 for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    for article in soup.select("#_main > article > ul:nth-child(3) > li"):
-        title = article.find("a").text.strip()
-        link = url.rstrip("/") + article.find("a")["href"]
+        for article in soup.select("div.post-list--item"):
+            title = article.select_one("div.post-list--item--title").text.strip()
+            link = url.rstrip("/") + article.select_one("a")["href"]
+            timestamp = article.select_one("div.post-list--item--date").text.strip()
 
-        post_data = {
-            "title": title,
-            "link": link
-        }
+            post_data = {
+                "title": title,
+                "link": link,
+                "timestamp": timestamp
+            }
 
-        output_data.append(post_data)
+            output_data.append(post_data)
+    except Exception as e:
+        print(f"Error while scraping {url}: {e}")
+
+output_data.sort(key=lambda x: x["timestamp"], reverse=True)
+output_data = output_data[:3]
+
 
 output_data = sorted(output_data, key=lambda x: x['link'], reverse=True)[:3]
 
