c914a00df6dd945476f507d8df83575614dfb824
devYuMinKim <gimyumin40@gmail.com>
2023-07-02 16:25:57 +0900

    Update blog_posts_scraper.py

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from dateutil.parser import parse
 
 urls = ["https://devyuminkim.github.io/lifelog/", "https://devyuminkim.github.io/devlog/"]
 
@@ -10,16 +11,17 @@ for url in urls:
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
+        articles = soup.select("#_main > article > ul:nth-child(3) > li")
 
-        for article in soup.select("ul.post-list-wrap > li"):
+        for article in articles[:3]:
             title = article.select_one("a").text.strip()
             link = url.rstrip("/") + article.select_one("a")["href"]
-            timestamp = article.select_one("span.post-meta").text.strip()
+            timestamp = article.select_one(".post-meta").text.strip()
 
             post_data = {
                 "title": title,
                 "link": link,
-                "timestamp": timestamp
+                "timestamp": parse(timestamp)
             }
 
             output_data.append(post_data)
@@ -28,6 +30,10 @@ for url in urls:
 
 output_data.sort(key=lambda x: x["timestamp"], reverse=True)
 output_data = output_data[:3]
+
+# Convert datetime objects back to strings
+for post in output_data:
+    post["timestamp"] = post["timestamp"].strftime("%Y-%m-%d")
 
 with open("output.json", "w") as json_file:
     json.dump(output_data, json_file)
