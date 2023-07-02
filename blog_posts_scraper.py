724d39cd0e392db79c1a8d0c7a3222c9784b4737
devYuMinKim <gimyumin40@gmail.com>
2023-07-02 15:56:02 +0900

    Update blog_posts_scraper.py

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -7,10 +7,11 @@ urls = ["https://devyuminkim.github.io/lifelog/", "https://devyuminkim.github.io
 output_data = []
 
 for url in urls:
+    blog_posts = []
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    for article in soup.select("#_main > article > ul:nth-child(3) > li")[:3]:
+    for article in soup.select("#_main > article > ul:nth-child(3) > li"):
         title = article.find("a").text.strip()
         link = url.rstrip("/") + article.find("a")["href"]
 
@@ -19,7 +20,11 @@ for url in urls:
             "link": link
         }
 
-        output_data.append(post_data)
+        blog_posts.append(post_data)
+    
+    output_data.extend(sorted(blog_posts, key=lambda x: x['link'], reverse=True)[:3])
+
+output_data = sorted(output_data, key=lambda x: x['link'], reverse=True)[:3]
 
 with open("output.json", "w") as json_file:
     json.dump(output_data, json_file)
