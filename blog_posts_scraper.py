3f86909313f372e3244810ecb48885384d69797d
devYuMinKim <gimyumin40@gmail.com>
2023-07-02 15:51:15 +0900

    Update blog_posts_scraper.py

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -10,9 +10,9 @@ for url in urls:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    for article in soup.select("div.item-thumbnail a")[:3]:
-        title = article.find("h2", class_="post-title").text.strip()
-        link = url.rstrip("/") + article["href"]
+    for article in soup.select("#_main > article > ul:nth-child(3) > li")[:3]:
+        title = article.find("a").text.strip()
+        link = url.rstrip("/") + article.find("a")["href"]
 
         post_data = {
             "title": title,
