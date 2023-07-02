3b32b76aca499da4c5479b03f2896d1e23e60b89
devYuMinKim <gimyumin40@gmail.com>
2023-07-03 00:35:18 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -4,13 +4,13 @@ import re
 with open("output.json", "r") as json_file:
     post_data = json.load(json_file)
 
-new_section = "<h2>Blog Posts</h2>\n\n"+''.join([f"- [{post['title']}]({post['link']}) {post['timestamp']}\n" for post in post_data])
+new_section = "<h2>Blog Posts</h2>\n\n" + ''.join([f"- [{post['title']}]({post['link']}) {post['timestamp']}\n" for post in post_data])
 
 with open("README.md", "r") as readme:
     readme_content = readme.read()
 
-pattern = re.compile(r'<h2>Blog\sPosts<\/h2>[\s\S]*?\n[^\-\#]', re.DOTALL)
-readme_content = re.sub(pattern, f"{new_section}\n", readme_content)
+pattern = re.compile(r"(<h2>Blog\sPosts<\/h2>\n)((?:-.*?)*)", re.DOTALL)
+readme_content = re.sub(pattern, new_section, readme_content)
 
 with open("README.md", "w") as readme:
     readme.write(readme_content)
