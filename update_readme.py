ab9cb63dd0e5a4f34b5477c12f00965e6a9dbe3f
devYuMinKim <gimyumin40@gmail.com>
2023-07-03 00:59:46 +0900

    Update

--- update_readme.py
+++ update_readme.py
@@ -4,7 +4,7 @@ def update_readme(posts):
     with open("README_template.md", "r") as template:
         template_content = template.read()
 
-    new_section = ''.join([f"- [{post['title']}]({post['link']}) {post['timestamp']}\n" for post in posts])
+    new_section = ''.join([f"- [{post['title']}]({post['link']})\n" for post in posts])
     readme_content = template_content.replace("{blog_posts}", new_section)
 
     with open("README.md", "w") as readme:
