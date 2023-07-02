f94699aebb1e7789b24dbdc54850d3855f096db4
devYuMinKim <gimyumin40@gmail.com>
2023-07-03 00:48:35 +0900

    Update

--- update_readme.py
+++ update_readme.py
@@ -1,17 +1,12 @@
-import json
-import re
+def update_readme(posts):
+    with open("README_template.md", "r") as template:
+        template_content = template.read()
 
-with open("output.json", "r") as json_file:
-    post_data = json.load(json_file)
+    new_section = ''.join([f"- [{post['title']}]({post['link']}) {post['timestamp']}\n" for post in posts])
+    readme_content = template_content.replace("{blog_posts}", new_section)
 
-new_section = "<h2>Blog Posts</h2>\n\n" + ''.join([f"- [{post['title']}]({post['link']}) {post['timestamp']}\n" for post in post_data])
+    with open("README.md", "w") as readme:
+        readme.write(readme_content)
+    print(f"README.md 업데이트 성공!")
 
-with open("README.md", "r") as readme:
-    readme_content = readme.read()
-
-pattern = re.compile(r"<h2>Blog\sPosts<\/h2>[^\#\-\r\n]*(?:\r?\n-.*?)*(?=\r?\n\#)", re.MULTILINE | re.DOTALL)
-readme_content = re.sub(pattern, new_section, readme_content)
-
-with open("README.md", "w") as readme:
-    readme.write(readme_content)
-print(f"README.md 업데이트 성공!")
+update_readme(sample_output_data)
