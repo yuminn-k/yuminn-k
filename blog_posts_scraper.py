62dba65132ded1d58445e29603807f2d8fe5a391
devYuMinKim <gimyumin40@gmail.com>
2023-07-03 00:17:23 +0900

    Update blog_posts_scraper.py

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from dateutil.parser import parse
+import os
+import re
+import random
 
 def is_absolute(url):
     return bool(requests.utils.urlparse(url).netloc)
@@ -17,14 +20,23 @@ for url in urls:
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
-        articles = soup.select("body > article > header > h1 > a")
+        articles = soup.select("#_main > article > ul:not(:first-child) > li.h4 > a")
 
-        for article in articles[:3]:
-            title = article.text.strip()
+        for article in articles:
+            title = article.find("span").text.strip()
             link = article["href"]
-            timestamp = article.find_previous_sibling("time").text.strip()
+
+            time_element = article.parent.find("time")
+            if time_element:
+                timestamp = time_element.text.strip()
+            else:
+                print(f"Timestamp not found for article: {title}")
+                continue
+
             if not is_absolute(link):
-                link = url.rstrip("/") + "/" + link.lstrip("/")
+                link = url.rstrip("/") + link.lstrip(".")
+                # 중복되는 부분 제거
+                link = re.sub(r"/(?:devlog|lifelog)/", "/", link, count=1)
 
             print(f"title: {title}, link: {link}, timestamp: {timestamp}")
 
@@ -38,15 +50,26 @@ for url in urls:
     except Exception as e:
         print(f"Error while scraping {url}: {e}")
 
-output_data.sort(key=lambda x: x["timestamp"], reverse=True)
-output_data = output_data[:3]
+# 랜덤한 3개의 포스트를 가져옵니다.
+sample_output_data = random.sample(output_data, min(len(output_data), 3))
+sample_output_data.sort(key=lambda x: x["timestamp"], reverse=True)
 
-for post in output_data:
+for post in sample_output_data:
     post["timestamp"] = post["timestamp"].strftime("%Y-%m-%d")
 
 try:
-    with open("output.json", "w") as json_file:
-        json.dump(output_data, json_file)
-    print("output.json에 성공적으로 저장되었습니다.")
+    output_file = "output.json"
+    with open(output_file, "w") as json_file:
+        json.dump(sample_output_data, json_file)
+    print(f"output.json에 성공적으로 저장되었습니다. 작업 디렉토리: {os.getcwd()}")
 except Exception as e:
     print(f"output.json에 저장하는 동안 오류가 발생했습니다: {e}")
+
+# output.json이 있는지 확인하는 코드 추가
+print(f"\n저장된 output.json 파일의 경로: {os.path.abspath(output_file)}")
+
+# output.json 파일 내용 확인 코드 추가
+print("\noutput.json 파일 내용 확인:")
+with open(output_file, "r") as json_file:
+    loaded_data = json.load(json_file)
+    print(json.dumps(loaded_data, indent=4))
