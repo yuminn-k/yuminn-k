95907a93cc9952790cca75eba51e83e5810883e7
devYuMinKim <gimyumin40@gmail.com>
2023-07-03 00:21:57 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -6,13 +6,13 @@ with open("output.json", "r") as json_file:
 
 new_section = "<h2>Blog Posts</h2>\n\n"
 for post in post_data:
-    new_section += f"- [{post['title']}]({post['link']})\n"
+    new_section += f"- [{post['title']}]({post['link']}) {post['timestamp']}\n"
 
 with open("README.md", "r") as readme:
     readme_content = readme.read()
 
-pattern = re.compile(r"<h2>Blog Posts<\/h2>.*?<h2>", re.DOTALL)
-readme_content = re.sub(pattern, new_section + "<h2>", readme_content)
+pattern = re.compile(r"<h2>Blog Posts<\/h2>\n[\s\S]*?(?=!)(?:1|\s){0,2}\n", re.MULTILINE)
+readme_content = re.sub(pattern, new_section + "\n", readme_content)
 
 with open("README.md", "w") as readme:
     readme.write(readme_content)
