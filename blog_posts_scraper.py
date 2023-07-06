da07226b52b1ef6b740b5ba298fe8408feeb1510
devYuMinKim <gimyumin40@gmail.com>
2023-07-06 14:18:36 +0900

    🎨 refactor : Improve logic to make it available to other users

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -1,75 +1,65 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-from dateutil.parser import parse
 import os
-import re
 import random
+from dotenv import load_dotenv
 
 def is_absolute(url):
     return bool(requests.utils.urlparse(url).netloc)
 
-urls = [
-    "https://devyuminkim.github.io/lifelog/",
-    "https://devyuminkim.github.io/devlog/",
-]
+def get_random_blog_posts(url, css_selector):
+    output_data = []
 
-output_data = []
-
-for url in urls:
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
-        articles = soup.select("#_main > article > ul:not(:first-child) > li.h4 > a")
+        articles = soup.select(css_selector + ' > a')
 
         for article in articles:
-            title = article.find("span").text.strip()
+            title = article.text.strip()
             link = article["href"]
-
-            time_element = article.parent.find("time")
-            if time_element:
-                timestamp = time_element.text.strip()
-            else:
-                print(f"Timestamp not found for article: {title}")
-                continue
 
             if not is_absolute(link):
                 link = url.rstrip("/") + link.lstrip(".")
-                # 중복되는 부분 제거
-                link = re.sub(r"/(?:devlog|lifelog)/", "/", link, count=1)
-
-            print(f"title: {title}, link: {link}, timestamp: {timestamp}")
 
             post_data = {
                 "title": title,
                 "link": link,
-                "timestamp": parse(timestamp),
             }
 
             output_data.append(post_data)
     except Exception as e:
         print(f"Error while scraping {url}: {e}")
 
-# 랜덤한 3개의 포스트를 가져옵니다.
-sample_output_data = random.sample(output_data, min(len(output_data), 3))
-sample_output_data.sort(key=lambda x: x["timestamp"], reverse=True)
+    return output_data
 
-for post in sample_output_data:
-    post["timestamp"] = post["timestamp"].strftime("%Y-%m-%d")
+def save_output_to_json(sample_output_data, output_file):
+    try:
+        with open(output_file, "w") as json_file:
+            json.dump(sample_output_data, json_file)
+        print(f"{output_file}에 성공적으로 저장되었습니다. 작업 디렉토리: {os.getcwd()}")
+    except Exception as e:
+        print(f"{output_file}에 저장하는 동안 오류가 발생했습니다: {e}\n")
+    
+    return output_file
 
-try:
+def main():
+    load_dotenv()
+    url = os.getenv("URL")
+    css_selector = os.getenv("CSS_SELECTOR")
     output_file = "output.json"
-    with open(output_file, "w") as json_file:
-        json.dump(sample_output_data, json_file)
-    print(f"output.json에 성공적으로 저장되었습니다. 작업 디렉토리: {os.getcwd()}")
-except Exception as e:
-    print(f"output.json에 저장하는 동안 오류가 발생했습니다: {e}")
 
-# output.json이 있는지 확인하는 코드 추가
-print(f"\n저장된 output.json 파일의 경로: {os.path.abspath(output_file)}")
+    output_data = get_random_blog_posts(url, css_selector)
+    sample_output_data = random.sample(output_data, min(len(output_data), 3))
+    save_output_to_json(sample_output_data, output_file)
 
-# output.json 파일 내용 확인 코드 추가
-print("\noutput.json 파일 내용 확인:")
-with open(output_file, "r") as json_file:
-    loaded_data = json.load(json_file)
-    print(json.dumps(loaded_data, indent=4))
+    print(f"\n저장된 {output_file} 파일의 경로: {os.path.abspath(output_file)}")
+
+    print(f"\n{output_file} 파일 내용 확인:")
+    with open(output_file, "r") as json_file:
+        loaded_data = json.load(json_file)
+        print(json.dumps(loaded_data, indent=4))
+
+if __name__ == "__main__":
+    main()
