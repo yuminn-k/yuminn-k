8c39172f248d5d729ce3351b741e9cb6f1c3b336
devYuMinKim <gimyumin40@gmail.com>
2023-07-15 23:02:04 +0900

    Update logic

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -5,35 +5,25 @@ import os
 import random
 from pathlib import Path
 
-# URL이 절대 경로인지 확인하는 함수
 def is_absolute(url):
     return bool(requests.utils.urlparse(url).netloc)
 
-# 블로그에서 포스트 목록을 가져오는 함수
 def get_random_blog_posts(url, css_selector):
     output_data = []
 
     try:
-        # 블로그의 HTML 코드 가져오기
         response = requests.get(url)
-        response.raise_for_status()  # 에러 발생 시 예외를 던짐
+        response.raise_for_status()  
         soup = BeautifulSoup(response.content, "html.parser")
-
-        # 포스트 목록 가져오기
         articles = soup.select(css_selector)
-
-        # 모든 <a> 태그를 저장하기 위한 리스트
         all_links = []
 
-        # 각 포스트의 a 태그들의 링크를 가져옵니다.
         for article in articles:
             links = article.find_all("a")
             all_links.extend(links)
 
-        # 모든 <a> 태그들 중에서 무작위로 3개를 선택합니다.
         selected_links = random.sample(all_links, min(3, len(all_links)))
 
-        # 각 링크의 제목과 링크 저장하기
         for link in selected_links:
             title = link.text.strip()
             href = link["href"]
@@ -52,7 +42,6 @@ def get_random_blog_posts(url, css_selector):
 
     return output_data
 
-# 출력 데이터를 JSON 파일에 저장하는 함수
 def save_output_to_json(sample_output_data, output_file):
     output_path = Path(output_file)
     try:
@@ -66,14 +55,11 @@ def save_output_to_json(sample_output_data, output_file):
 
 def main():
     url = os.getenv("URL")
-    # url = "https://devyuminkim-devlog.vercel.app"
     css_selector = os.getenv("CSS_SELECTOR")
-    # css_selector = "#__next>div:nth-child(2)>div>div.notion-frame>div>main>div.notion-page-content>article>div.notion-collection.notion-block-52c3f72df427430ca4768e53c36a61c6>div:nth-child(2)>div>div"
     output_file = "output.json"
 
     output_data = get_random_blog_posts(url, css_selector)
-    print(f"Output Data: {output_data}")
-    sample_output_data = random.sample(output_data, min(len(output_data), 3)) # 기본 3개
+    sample_output_data = random.sample(output_data, min(len(output_data), 3))
     save_output_to_json(sample_output_data, output_file)
 
 if __name__ == "__main__":
--- update_readme.py
+++ update_readme.py
@@ -11,11 +11,7 @@ def update_readme(posts):
     readme_path = Path("README.md")
     try:
         template_content = template_path.read_text()
-
-        # 포스트 데이터를 원하는 형식으로 변환합니다.
         formatted_posts = '\n'.join(format_post(post) for post in posts)
-
-        # 출력 데이터를 템플릿에 적용하여 README.md 파일 생성
         with readme_path.open("w") as readme_file:
             readme_file.write(template_content.format(posts=formatted_posts))
         print("README.md 업데이트 성공!")
@@ -25,11 +21,9 @@ def update_readme(posts):
 def main():
     output_file = "output.json"
 
-    # JSON 파일에서 데이터 읽기
     with open(output_file) as json_file:
         data = json.load(json_file)
 
-    # README.md 파일 업데이트
     update_readme(data)
 
 if __name__ == "__main__":
