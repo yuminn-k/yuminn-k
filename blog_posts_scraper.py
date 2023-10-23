22ff7a4ea41668cd8737aef7f4bd06a69c6eb585
ユミン <55650732+yuminn-k@users.noreply.github.com>
2023-10-23 19:49:24 +0900

    refactor: Update blog_posts_scraper.py

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -8,6 +8,45 @@ from pathlib import Path
 def is_absolute(url):
     return bool(requests.utils.urlparse(url).netloc)
 
+# def get_random_blog_posts(url, css_selector):
+#     output_data = []
+
+#     try:
+#         response = requests.get(url)
+#         response.raise_for_status()  
+#         soup = BeautifulSoup(response.content, "html.parser")
+#         articles = soup.select(css_selector)
+#         all_links = []
+
+#         for article in articles:
+#             links = article.find_all("a")
+#             all_links.extend(links)
+
+#         selected_links = random.sample(all_links, min(3, len(all_links)))
+
+#         for link in selected_links:
+#             title = link.text.strip()
+
+#             if '202' in title:
+#                 index_202 = title.index('202')
+#                 title = title[:index_202]
+
+#             href = link["href"]
+        
+#             if not is_absolute(href):
+#                 href = url.rstrip("/") + href.lstrip(".")
+        
+#             post_data = {
+#                 "title": title,
+#                 "url": href,
+#             }
+
+#             output_data.append(post_data)
+#     except requests.exceptions.RequestException as e:
+#         raise RuntimeError(f"Error while scraping {url}: {e}")
+
+#     return output_data
+
 def get_random_blog_posts(url, css_selector):
     output_data = []
 
@@ -25,23 +64,34 @@ def get_random_blog_posts(url, css_selector):
         selected_links = random.sample(all_links, min(3, len(all_links)))
 
         for link in selected_links:
-            title = link.text.strip()
+            # Get the title
+            title_element = link.select_one("div.notion-collection-card-body > div:nth-child(1) > span > span > span > span")
+            title = title_element.text.strip() if title_element else ""
 
-            if '202' in title:
-                index_202 = title.index('202')
-                title = title[:index_202]
+            # Get the summary
+            summary_element = link.select_one("div.notion-collection-card-body > div:nth-child(2)")
+            summary = summary_element.text.strip() if summary_element else ""
 
-            href = link["href"]
-        
-            if not is_absolute(href):
-                href = url.rstrip("/") + href.lstrip(".")
-        
-            post_data = {
-                "title": title,
-                "url": href,
-            }
-
-            output_data.append(post_data)
+            # Get the date
+            date_element = link.select_one("div.notion-collection-card-body > div:nth-child(3)")
+            
+            if date_element and '202' in date_element.text:
+                index_202 = date_element.text.index('202')
+                date_text= date_element.text[:index_202]
+            
+                href=link["href"]
+                
+                if not is_absolute(href):
+                    href=url.rstrip("/") + href.lstrip(".")
+                
+                post_data={
+                    "title":title,
+                    "summary":summary,
+                    "date":date_text,
+                    "url":href,
+                }
+                
+                output_data.append(post_data)
     except requests.exceptions.RequestException as e:
         raise RuntimeError(f"Error while scraping {url}: {e}")
 
