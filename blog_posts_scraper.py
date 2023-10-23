feab340ca5850e9460f8ed2328ed455846bf9108
ユミン <55650732+yuminn-k@users.noreply.github.com>
2023-10-23 20:20:31 +0900

    refactor: Remove unnecessary code

--- blog_posts_scraper.py
+++ blog_posts_scraper.py
@@ -8,45 +8,6 @@ from pathlib import Path
 def is_absolute(url):
     return bool(requests.utils.urlparse(url).netloc)
 
-# def get_random_blog_posts(url, css_selector):
-#     output_data = []
-
-#     try:
-#         response = requests.get(url)
-#         response.raise_for_status()  
-#         soup = BeautifulSoup(response.content, "html.parser")
-#         articles = soup.select(css_selector)
-#         all_links = []
-
-#         for article in articles:
-#             links = article.find_all("a")
-#             all_links.extend(links)
-
-#         selected_links = random.sample(all_links, min(3, len(all_links)))
-
-#         for link in selected_links:
-#             title = link.text.strip()
-
-#             if '202' in title:
-#                 index_202 = title.index('202')
-#                 title = title[:index_202]
-
-#             href = link["href"]
-        
-#             if not is_absolute(href):
-#                 href = url.rstrip("/") + href.lstrip(".")
-        
-#             post_data = {
-#                 "title": title,
-#                 "url": href,
-#             }
-
-#             output_data.append(post_data)
-#     except requests.exceptions.RequestException as e:
-#         raise RuntimeError(f"Error while scraping {url}: {e}")
-
-#     return output_data
-
 def get_random_blog_posts(url, css_selector):
     output_data = []
 
